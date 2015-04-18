1a5f048a75ef56747fea245c2f79d173264cb156
gitwinded <gitwinded@users.noreply.github.com>
2015-04-18 17:11:04 -0500

    delete todo.md and put checkboxes on to do list in main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,23 @@
 -- main.lua
 --To Do:
---1x Define the character's direction of travel.
---2x Call function collidable()
---3x Define drawChar() function
---4x check direction the character is moving for drawChar()
---5x use ../tutorials/lovewikitutorials/grid to set motion rates
---6x use above to draw actual vs grid character positions
---7x Reflect left image into right in code
---8 Turn all character related variables into tables
---9 Create character Class using MiddleClass
---10 Figure out how to use love.keyboard.isDown instead of keypressed for moveChar()
---11 Use multiple frames to make character 'walk'
---12x Make map collidable function
---13x Make entity collidable function
---14x Set Name for Game Window
---15 Separate Entity and Map loading
---16 Create grass map from ff art
---17 Place entities on map from ff art
---18x START BACKING UP INTO GITHUB!!
+-- [x] Define the character's direction of travel.
+-- [x] Call function collidable()
+-- [x] Define drawChar() function
+-- [x] check direction the character is moving for drawChar()
+-- [x] use ../tutorials/lovewikitutorials/grid to set motion rates
+-- [x] use above to draw actual vs grid character positions
+-- [x] Reflect left image into right in code
+-- [ ] turn all character related variables into tables
+-- [ ] create character Class using MiddleClass
+-- [ ] Figure out how to use love.keyboard.isDown instead of keypressed for moveChar()
+-- [ ] Use multiple frames to make character 'walk'
+-- [x] Make map collidable function
+-- [x] Make entity collidable function
+-- [x] Set Name for Game Window
+-- [ ] Separate Entity and Map loading
+-- [ ] Create grass map from ff art
+-- [ ] Place entities on map from ff art
+-- [x] Back up into Github
 
 require 'map-functions'
 require 'char-functions'
